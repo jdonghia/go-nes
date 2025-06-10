d7a1235c4da8486ef27ccb8efa00780ec79faec6
João Donghia <joao.donghia@gmail.com>
2025-06-10 14:19:03 -0300

    check how clock and cycle works WIP

--- main.go
+++ main.go
@@ -12,33 +12,64 @@ type Nes struct {
 }
 
 type Bus struct {
-	write func(RAM *[0xffff]uint8, data uint8, addr uint16)
-	read  func(RAM *[0xffff]uint8, addr uint16) uint8
+	write func(*Nes, uint8, uint16)
+	read  func(*[0xffff]uint8, uint16) uint8
 }
 
 type Cpu struct {
-	pc uint16
-	sp uint16
-	a  uint8
-	x  uint8
-	y  uint8
-	sr uint8
+	pc     uint16
+	sp     uint16
+	a      uint8
+	x      uint8
+	y      uint8
+	cycles uint8
+	flags  Flags
+	byte   uint8
+	addr   uint16
 }
 
-const (
-	FLAGS = iota
-	C     = (1 << 0)
-	Z     = (1 << 1)
-	I     = (1 << 2)
-	D     = (1 << 3)
-	B     = (1 << 4)
-	U     = (1 << 5)
-	V     = (1 << 6)
-	N     = (1 << 7)
-)
+type Flags struct {
+	C uint8
+	Z uint8
+	I uint8
+	D uint8
+	B uint8
+	U uint8
+	V uint8
+	N uint8
+}
 
-func write(RAM *[0xffff]uint8, data uint8, addr uint16) {
-	RAM[addr] = data
+type Instruction struct {
+	name      string
+	operate   func(*Nes)
+	addr_mode func(*Nes)
+	cycles    uint8
+}
+
+var lookup = []Instruction{
+	{"BRK", BRK, IMM, 7}, {"ORA", ORA, IZX, 6}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 3}, {"ORA", ORA, ZP0, 3}, {"ASL", ASL, ZP0, 5}, {"???", XXX, IMP, 5}, {"PHP", PHP, IMP, 3}, {"ORA", ORA, IMM, 2}, {"ASL", ASL, IMP, 2}, {"???", XXX, IMP, 2}, {"???", NOP, IMP, 4}, {"ORA", ORA, ABS, 4}, {"ASL", ASL, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BPL", BPL, REL, 2}, {"ORA", ORA, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"ORA", ORA, ZPX, 4}, {"ASL", ASL, ZPX, 6}, {"???", XXX, IMP, 6}, {"CLC", CLC, IMP, 2}, {"ORA", ORA, ABY, 4}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"ORA", ORA, ABX, 4}, {"ASL", ASL, ABX, 7}, {"???", XXX, IMP, 7},
+	{"JSR", JSR, ABS, 6}, {"AND", AND, IZX, 6}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"BIT", BIT, ZP0, 3}, {"AND", AND, ZP0, 3}, {"ROL", ROL, ZP0, 5}, {"???", XXX, IMP, 5}, {"PLP", PLP, IMP, 4}, {"AND", AND, IMM, 2}, {"ROL", ROL, IMP, 2}, {"???", XXX, IMP, 2}, {"BIT", BIT, ABS, 4}, {"AND", AND, ABS, 4}, {"ROL", ROL, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BMI", BMI, REL, 2}, {"AND", AND, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"AND", AND, ZPX, 4}, {"ROL", ROL, ZPX, 6}, {"???", XXX, IMP, 6}, {"SEC", SEC, IMP, 2}, {"AND", AND, ABY, 4}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"AND", AND, ABX, 4}, {"ROL", ROL, ABX, 7}, {"???", XXX, IMP, 7},
+	{"RTI", RTI, IMP, 6}, {"EOR", EOR, IZX, 6}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 3}, {"EOR", EOR, ZP0, 3}, {"LSR", LSR, ZP0, 5}, {"???", XXX, IMP, 5}, {"PHA", PHA, IMP, 3}, {"EOR", EOR, IMM, 2}, {"LSR", LSR, IMP, 2}, {"???", XXX, IMP, 2}, {"JMP", JMP, ABS, 3}, {"EOR", EOR, ABS, 4}, {"LSR", LSR, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BVC", BVC, REL, 2}, {"EOR", EOR, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"EOR", EOR, ZPX, 4}, {"LSR", LSR, ZPX, 6}, {"???", XXX, IMP, 6}, {"CLI", CLI, IMP, 2}, {"EOR", EOR, ABY, 4}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"EOR", EOR, ABX, 4}, {"LSR", LSR, ABX, 7}, {"???", XXX, IMP, 7},
+	{"RTS", RTS, IMP, 6}, {"ADC", ADC, IZX, 6}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 3}, {"ADC", ADC, ZP0, 3}, {"ROR", ROR, ZP0, 5}, {"???", XXX, IMP, 5}, {"PLA", PLA, IMP, 4}, {"ADC", ADC, IMM, 2}, {"ROR", ROR, IMP, 2}, {"???", XXX, IMP, 2}, {"JMP", JMP, IND, 5}, {"ADC", ADC, ABS, 4}, {"ROR", ROR, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BVS", BVS, REL, 2}, {"ADC", ADC, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"ADC", ADC, ZPX, 4}, {"ROR", ROR, ZPX, 6}, {"???", XXX, IMP, 6}, {"SEI", SEI, IMP, 2}, {"ADC", ADC, ABY, 4}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"ADC", ADC, ABX, 4}, {"ROR", ROR, ABX, 7}, {"???", XXX, IMP, 7},
+	{"???", NOP, IMP, 2}, {"STA", STA, IZX, 6}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 6}, {"STY", STY, ZP0, 3}, {"STA", STA, ZP0, 3}, {"STX", STX, ZP0, 3}, {"???", XXX, IMP, 3}, {"DEY", DEY, IMP, 2}, {"???", NOP, IMP, 2}, {"TXA", TXA, IMP, 2}, {"???", XXX, IMP, 2}, {"STY", STY, ABS, 4}, {"STA", STA, ABS, 4}, {"STX", STX, ABS, 4}, {"???", XXX, IMP, 4},
+	{"BCC", BCC, REL, 2}, {"STA", STA, IZY, 6}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 6}, {"STY", STY, ZPX, 4}, {"STA", STA, ZPX, 4}, {"STX", STX, ZPY, 4}, {"???", XXX, IMP, 4}, {"TYA", TYA, IMP, 2}, {"STA", STA, ABY, 5}, {"TXS", TXS, IMP, 2}, {"???", XXX, IMP, 5}, {"???", NOP, IMP, 5}, {"STA", STA, ABX, 5}, {"???", XXX, IMP, 5}, {"???", XXX, IMP, 5},
+	{"LDY", LDY, IMM, 2}, {"LDA", LDA, IZX, 6}, {"LDX", LDX, IMM, 2}, {"???", XXX, IMP, 6}, {"LDY", LDY, ZP0, 3}, {"LDA", LDA, ZP0, 3}, {"LDX", LDX, ZP0, 3}, {"???", XXX, IMP, 3}, {"TAY", TAY, IMP, 2}, {"LDA", LDA, IMM, 2}, {"TAX", TAX, IMP, 2}, {"???", XXX, IMP, 2}, {"LDY", LDY, ABS, 4}, {"LDA", LDA, ABS, 4}, {"LDX", LDX, ABS, 4}, {"???", XXX, IMP, 4},
+	{"BCS", BCS, REL, 2}, {"LDA", LDA, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 5}, {"LDY", LDY, ZPX, 4}, {"LDA", LDA, ZPX, 4}, {"LDX", LDX, ZPY, 4}, {"???", XXX, IMP, 4}, {"CLV", CLV, IMP, 2}, {"LDA", LDA, ABY, 4}, {"TSX", TSX, IMP, 2}, {"???", XXX, IMP, 4}, {"LDY", LDY, ABX, 4}, {"LDA", LDA, ABX, 4}, {"LDX", LDX, ABY, 4}, {"???", XXX, IMP, 4},
+	{"CPY", CPY, IMM, 2}, {"CMP", CMP, IZX, 6}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 8}, {"CPY", CPY, ZP0, 3}, {"CMP", CMP, ZP0, 3}, {"DEC", DEC, ZP0, 5}, {"???", XXX, IMP, 5}, {"INY", INY, IMP, 2}, {"CMP", CMP, IMM, 2}, {"DEX", DEX, IMP, 2}, {"???", XXX, IMP, 2}, {"CPY", CPY, ABS, 4}, {"CMP", CMP, ABS, 4}, {"DEC", DEC, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BNE", BNE, REL, 2}, {"CMP", CMP, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"CMP", CMP, ZPX, 4}, {"DEC", DEC, ZPX, 6}, {"???", XXX, IMP, 6}, {"CLD", CLD, IMP, 2}, {"CMP", CMP, ABY, 4}, {"NOP", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"CMP", CMP, ABX, 4}, {"DEC", DEC, ABX, 7}, {"???", XXX, IMP, 7},
+	{"CPX", CPX, IMM, 2}, {"SBC", SBC, IZX, 6}, {"???", NOP, IMP, 2}, {"???", XXX, IMP, 8}, {"CPX", CPX, ZP0, 3}, {"SBC", SBC, ZP0, 3}, {"INC", INC, ZP0, 5}, {"???", XXX, IMP, 5}, {"INX", INX, IMP, 2}, {"SBC", SBC, IMM, 2}, {"NOP", NOP, IMP, 2}, {"???", SBC, IMP, 2}, {"CPX", CPX, ABS, 4}, {"SBC", SBC, ABS, 4}, {"INC", INC, ABS, 6}, {"???", XXX, IMP, 6},
+	{"BEQ", BEQ, REL, 2}, {"SBC", SBC, IZY, 5}, {"???", XXX, IMP, 2}, {"???", XXX, IMP, 8}, {"???", NOP, IMP, 4}, {"SBC", SBC, ZPX, 4}, {"INC", INC, ZPX, 6}, {"???", XXX, IMP, 6}, {"SED", SED, IMP, 2}, {"SBC", SBC, ABY, 4}, {"NOP", NOP, IMP, 2}, {"???", XXX, IMP, 7}, {"???", NOP, IMP, 4}, {"SBC", SBC, ABX, 4}, {"INC", INC, ABX, 7}, {"???", XXX, IMP, 7},
+}
+
+func init() {
+}
+
+func write(nes *Nes, byte uint8, addr uint16) {
+	nes.RAM[addr] = byte
 }
 
 func read(RAM *[0xffff]uint8, addr uint16) uint8 {
@@ -46,17 +77,17 @@ func read(RAM *[0xffff]uint8, addr uint16) uint8 {
 }
 
 func main() {
+
 	nes := Nes{
 		cpu: Cpu{},
 		bus: Bus{
 			write: write,
 			read:  read,
 		},
-		RAM: [0xffff]uint8{}}
+		RAM: [0xffff]uint8{0xad, 0x12, 0x34, 0x42}}
 
-	// nes.bus.write(&nes.RAM, 0x42, 0x1234)
+	ticker := time.NewTicker(1000 * time.Millisecond)
 
-	ticker := time.NewTicker(50 * time.Millisecond)
 	defer ticker.Stop()
 
 	for {
@@ -68,82 +99,116 @@ func main() {
 
 }
 
+var cycles uint8 = 0
+
 func clock(nes *Nes) {
-	nes.cpu.a++
-	fmt.Printf("0x%04x\n", nes.cpu.a)
-	println(nes.cpu.a)
+	fmt.Printf("Address: $%04X Data: $%02X \n", nes.cpu.pc, read(&nes.RAM, nes.cpu.pc))
+
+	opcode := read(&nes.RAM, nes.cpu.pc) // a5 lda
+
+	lookup[opcode].addr_mode(nes)
+
+	lookup[opcode].operate(nes)
+
+	nes.cpu.pc++
+
+	if cycles == 0 {
+
+		cycles = 4
+
+	}
+
+	cycles--
 }
 
-//Adressing Modes
-func IMP() uint8 { return 0 }
-func IMM() uint8 { return 0 }
-func ZP0() uint8 { return 0 }
-func ZPX() uint8 { return 0 }
-func ZPY() uint8 { return 0 }
-func REL() uint8 { return 0 }
-func ABS() uint8 { return 0 }
-func ABX() uint8 { return 0 }
-func ABY() uint8 { return 0 }
-func IND() uint8 { return 0 }
-func IZX() uint8 { return 0 }
-func IZY() uint8 { return 0 }
+// Addr Modes
+func IMP(nes *Nes) {}
+func IMM(nes *Nes) {
+	nes.cpu.addr = nes.cpu.pc + 1
+}
+
+func LDA(nes *Nes) {
+
+	nes.cpu.a = read(&nes.RAM, nes.cpu.addr)
+
+}
+
+func ABS(nes *Nes) {
+	lo := read(&nes.RAM, nes.cpu.pc)
+	nes.cpu.pc++
+
+	hi := read(&nes.RAM, nes.cpu.pc)
+	nes.cpu.pc++
+
+	nes.cpu.addr = uint16(lo) | (uint16(hi) << 8)
+	nes.cpu.pc = nes.cpu.addr
+}
+
+func ZP0(nes *Nes) {}
+func ZPX(nes *Nes) {}
+func ZPY(nes *Nes) {}
+func REL(nes *Nes) {}
+func ABX(nes *Nes) {}
+func ABY(nes *Nes) {}
+func IND(nes *Nes) {}
+func IZX(nes *Nes) {}
+func IZY(nes *Nes) {}
 
 // Instructions
-func ADC() uint8 { return 0 }
-func AND() uint8 { return 0 }
-func ASL() uint8 { return 0 }
-func BCC() uint8 { return 0 }
-func BCS() uint8 { return 0 }
-func BEQ() uint8 { return 0 }
-func BIT() uint8 { return 0 }
-func BMI() uint8 { return 0 }
-func BNE() uint8 { return 0 }
-func BPL() uint8 { return 0 }
-func BRK() uint8 { return 0 }
-func BVC() uint8 { return 0 }
-func BVS() uint8 { return 0 }
-func CLC() uint8 { return 0 }
-func CLD() uint8 { return 0 }
-func CLI() uint8 { return 0 }
-func CLV() uint8 { return 0 }
-func CMP() uint8 { return 0 }
-func CPX() uint8 { return 0 }
-func CPY() uint8 { return 0 }
-func DEC() uint8 { return 0 }
-func DEX() uint8 { return 0 }
-func DEY() uint8 { return 0 }
-func EOR() uint8 { return 0 }
-func INC() uint8 { return 0 }
-func INX() uint8 { return 0 }
-func INY() uint8 { return 0 }
-func JMP() uint8 { return 0 }
-func JSR() uint8 { return 0 }
-func LDA() uint8 { return 0 }
-func LDX() uint8 { return 0 }
-func LDY() uint8 { return 0 }
-func LSR() uint8 { return 0 }
-func NOP() uint8 { return 0 }
-func ORA() uint8 { return 0 }
-func PHA() uint8 { return 0 }
-func PHP() uint8 { return 0 }
-func PLA() uint8 { return 0 }
-func PLP() uint8 { return 0 }
-func ROL() uint8 { return 0 }
-func ROR() uint8 { return 0 }
-func RTI() uint8 { return 0 }
-func RTS() uint8 { return 0 }
-func SBC() uint8 { return 0 }
-func SEC() uint8 { return 0 }
-func SED() uint8 { return 0 }
-func SEI() uint8 { return 0 }
-func STA() uint8 { return 0 }
-func STX() uint8 { return 0 }
-func STY() uint8 { return 0 }
-func TAX() uint8 { return 0 }
-func TAY() uint8 { return 0 }
-func TSX() uint8 { return 0 }
-func TXA() uint8 { return 0 }
-func TXS() uint8 { return 0 }
-func TYA() uint8 { return 0 }
+func ADC(nes *Nes) {}
+func AND(nes *Nes) {}
+func ASL(nes *Nes) {}
+func BCC(nes *Nes) {}
+func BCS(nes *Nes) {}
+func BEQ(nes *Nes) {}
+func BIT(nes *Nes) {}
+func BMI(nes *Nes) {}
+func BNE(nes *Nes) {}
+func BPL(nes *Nes) {}
+func BRK(nes *Nes) {}
+func BVC(nes *Nes) {}
+func BVS(nes *Nes) {}
+func CLC(nes *Nes) {}
+func CLD(nes *Nes) {}
+func CLI(nes *Nes) {}
+func CLV(nes *Nes) {}
+func CMP(nes *Nes) {}
+func CPX(nes *Nes) {}
+func CPY(nes *Nes) {}
+func DEC(nes *Nes) {}
+func DEX(nes *Nes) {}
+func DEY(nes *Nes) {}
+func EOR(nes *Nes) {}
+func INC(nes *Nes) {}
+func INX(nes *Nes) {}
+func INY(nes *Nes) {}
+func JMP(nes *Nes) {}
+func JSR(nes *Nes) {}
+func LDX(nes *Nes) {}
+func LDY(nes *Nes) {}
+func LSR(nes *Nes) {}
+func NOP(nes *Nes) {}
+func ORA(nes *Nes) {}
+func PHA(nes *Nes) {}
+func PHP(nes *Nes) {}
+func PLA(nes *Nes) {}
+func PLP(nes *Nes) {}
+func ROL(nes *Nes) {}
+func ROR(nes *Nes) {}
+func RTI(nes *Nes) {}
+func RTS(nes *Nes) {}
+func SBC(nes *Nes) {}
+func SEC(nes *Nes) {}
+func SED(nes *Nes) {}
+func SEI(nes *Nes) {}
+func STA(nes *Nes) {}
+func STX(nes *Nes) {}
+func STY(nes *Nes) {}
+func TAX(nes *Nes) {}
+func TAY(nes *Nes) {}
+func TSX(nes *Nes) {}
+func TXA(nes *Nes) {}
+func TXS(nes *Nes) {}
+func TYA(nes *Nes) {}
 
-func XXX() uint8 { return 0 }
+func XXX(nes *Nes) {}
